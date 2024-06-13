5429b1215d716d93f1bb5914b6987864898d51a4
Francisco Diakomas <fdiakomas@gmail.com>
2024-06-13 07:02:16 -0300

    terminei o frontend

--- main.js
+++ main.js
@@ -22,6 +22,36 @@ window.onload = function () {
     btnsPagameto.forEach((iten)=>{
         iten.addEventListener("click",PagamentoBTN)
     })
+    const btnsPagameto1 = document.querySelectorAll(".navigator1 button")
+    function PagamentoBTN1() {
+        btnsPagameto1.forEach((btn)=>{
+            btn.classList.remove("active1")
+        })
+        this.classList.add("active1")
+    }
+    btnsPagameto1.forEach((iten)=>{
+        iten.addEventListener("click",PagamentoBTN1)
+    })    
+    const btnsPagameto2 = document.querySelectorAll(".navigator2 button")
+    function PagamentoBTN2() {
+        btnsPagameto2.forEach((btn)=>{
+            btn.classList.remove("active1")
+        })
+        this.classList.add("active1")
+    }
+    btnsPagameto2.forEach((iten)=>{
+        iten.addEventListener("click",PagamentoBTN2)
+    })    
+    const btnsPagameto3 = document.querySelectorAll(".navigator3 button")
+    function PagamentoBTN3() {
+        btnsPagameto3.forEach((btn)=>{
+            btn.classList.remove("active1")
+        })
+        this.classList.add("active1")
+    }
+    btnsPagameto3.forEach((iten)=>{
+        iten.addEventListener("click",PagamentoBTN3)
+    })    
 
 
      //tema do site
@@ -46,6 +76,88 @@ window.onload = function () {
         
     })
 
+
+
+     //gerenciamento da navegação ou rotas do meu site
+    const dashBoardsreen = document.getElementById("dash")
+    const cursoScreen = document.getElementById("curs")
+    const formador = document.getElementById("forms")
+    const alunos = document.getElementById("alns")
+    const pagamento = document.getElementById("pagamnt")
+    const admin = document.getElementById("adm")
+
+    const screndash = document.getElementById("inicial")
+    const screnCurs = document.getElementById("allcurs")
+    const screnForms = document.getElementById("formadores")
+    const screnLns = document.getElementById("alunos")
+    const screnPay = document.getElementById("pagamentos")
+    const screnAdmin = document.getElementById("Administrador")
+    let lista =[]
+
+    dashBoardsreen.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screndash.classList.add("show")
+        screndash.classList.remove("hiden")
+        lista = [screnCurs,screnForms,screnPay,screnLns,screnAdmin]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+    cursoScreen.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screnCurs.classList.add("show")
+        screnCurs.classList.remove("hiden")
+        lista = [screndash,screnForms,screnPay,screnLns,screnAdmin]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+    formador.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screnForms.classList.add("show")
+        screnForms.classList.remove("hiden")
+        lista = [screndash,screnCurs,screnPay,screnLns,screnAdmin]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
+    alunos.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screnLns.classList.add("show")
+        screnLns.classList.remove("hiden")
+        lista = [screndash,screnCurs,screnPay,screnForms,screnAdmin]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+    pagamento.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screnPay.classList.add("show")
+        screnPay.classList.remove("hiden")
+        lista = [screndash,screnCurs,screnLns,screnForms,screnAdmin]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+    admin.addEventListener("click",(e)=>{
+        e.preventDefault()
+        screnAdmin.classList.add("show")
+        screnAdmin.classList.remove("hiden")
+        lista = [screndash,screnCurs,screnLns,screnForms,screnPay]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
+
+
     //tela do gerenciamento dos cursos
 
     const btnCadastrarCurso = document.getElementById("cadastrarCurso")
@@ -53,6 +165,7 @@ window.onload = function () {
     const btnBuscarCurso= document.getElementById("buscarCurso")
     const btnDeletarCurso = document.getElementById("deletarCurso")
     const btnalterarCurso = document.getElementById("alterarCurso")
+    const titulopageCurso = document.getElementById("titulopageCurso")
 
     const screenCadastrarCurso = document.getElementsByClassName("cadastrar")[0]
     const screenListarCurso = document.getElementsByClassName("listar")[0]
@@ -63,98 +176,59 @@ window.onload = function () {
     btnCadastrarCurso.addEventListener("click",()=>{
         screenCadastrarCurso.classList.remove("hiden")
         screenCadastrarCurso.classList.add("show")
-        
-        screenBuscarCurso.classList.remove("show")
-        screenBuscarCurso.classList.add("hiden")
-
-        screenDeletarCurso.classList.remove("show")
-        screenDeletarCurso.classList.add("hiden")
-
-        screenListarCurso.classList.remove("show")
-        screenListarCurso.classList.add("hiden")
-
-        screenAlterarCurso.classList.remove("show")
-        screenAlterarCurso.classList.add("hiden")
+        titulopageCurso.textContent = "Cadastro de Curso"
+        lista = [ screenBuscarCurso, screenDeletarCurso,screenListarCurso,screenAlterarCurso]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
     btnBuscarCurso.addEventListener("click",()=>{
-
         screenBuscarCurso.classList.remove("hiden")
         screenBuscarCurso.classList.add("show")
-        
-        screenCadastrarCurso.classList.remove("show")
-        screenCadastrarCurso.classList.add("hiden")
-
-        screenDeletarCurso.classList.remove("show")
-        screenDeletarCurso.classList.add("hiden")
-
-        screenListarCurso.classList.remove("show")
-        screenListarCurso.classList.add("hiden")
-
-        screenAlterarCurso.classList.remove("show")
-        screenAlterarCurso.classList.add("hiden")
-
+        titulopageCurso.textContent = "Pesquisa de Curso"
+        lista = [ screenCadastrarCurso, screenDeletarCurso,screenListarCurso,screenAlterarCurso]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
     btnLitarCuros.addEventListener("click",()=>{
-
         screenListarCurso.classList.remove("hiden")
         screenListarCurso.classList.add("show")
-        screenListarCurso.style.flexDirection = "column"
-        screenListarCurso.style.gap = "10px"
-        
-        screenCadastrarCurso.classList.remove("show")
-        screenCadastrarCurso.classList.add("hiden")
-
-        screenDeletarCurso.classList.remove("show")
-        screenDeletarCurso.classList.add("hiden")
-
-        screenBuscarCurso.classList.remove("show")
-        screenBuscarCurso.classList.add("hiden")
-
-        screenAlterarCurso.classList.remove("show")
-        screenAlterarCurso.classList.add("hiden")
-
+        titulopageCurso.textContent = "Listagem de Curso"
+        lista = [ screenCadastrarCurso, screenDeletarCurso, screenBuscarCurso,screenAlterarCurso]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
     btnDeletarCurso.addEventListener("click",()=>{
-
         screenDeletarCurso.classList.remove("hiden")
         screenDeletarCurso.classList.add("show")
-        
-        screenCadastrarCurso.classList.remove("show")
-        screenCadastrarCurso.classList.add("hiden")
-
-        screenBuscarCurso.classList.remove("show")
-        screenBuscarCurso.classList.add("hiden")
-
-        screenListarCurso.classList.remove("show")
-        screenListarCurso.classList.add("hiden")
-
-        screenAlterarCurso.classList.remove("show")
-        screenAlterarCurso.classList.add("hiden")
-
+        titulopageCurso.textContent = "Remoção de Curso"
+        lista = [ screenCadastrarCurso, screenListarCurso, screenBuscarCurso,screenAlterarCurso]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
-
     btnalterarCurso.addEventListener("click",()=>{
-
         screenAlterarCurso.classList.remove("hiden")
         screenAlterarCurso.classList.add("show")
-        
-        screenCadastrarCurso.classList.remove("show")
-        screenCadastrarCurso.classList.add("hiden")
-
-        screenBuscarCurso.classList.remove("show")
-        screenBuscarCurso.classList.add("hiden")
-
-        screenListarCurso.classList.remove("show")
-        screenListarCurso.classList.add("hiden")
-
-        screenDeletarCurso.classList.remove("show")
-        screenDeletarCurso.classList.add("hiden")
-
+        titulopageCurso.textContent = "Actualização de Curso"
+        lista = [ screenCadastrarCurso, screenListarCurso, screenBuscarCurso,screenDeletarCurso]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
+
+    
 
 
   //tela do gerenciamento dos Formadores
@@ -170,100 +244,132 @@ window.onload = function () {
     const screenBuscarFormador = document.getElementsByClassName("BuscarFormador")[0]
     const screendeletarFormador = document.getElementsByClassName("deletarFormador")[0]
     const screenalterarFormador = document.getElementsByClassName("alterarFormador")[0]
+    
+    const titulopageFormadores = document.getElementById("titulopageFormadores")
+
 
     btncadastrarFormador.addEventListener("click",()=>{
         screencadastrarFormador.classList.remove("hiden")
         screencadastrarFormador.classList.add("show")
-        
-        screenBuscarFormador.classList.remove("show")
-        screenBuscarFormador.classList.add("hiden")
-
-        screendeletarFormador.classList.remove("show")
-        screendeletarFormador.classList.add("hiden")
-
-        screenlistarFormador.classList.remove("show")
-        screenlistarFormador.classList.add("hiden")
-
-        screenalterarFormador.classList.remove("show")
-        screenalterarFormador.classList.add("hiden")
+        titulopageFormadores.textContent = "Cadastro de Formador"
+        lista = [ screenlistarFormador,  screenBuscarFormador, screendeletarFormador,screenalterarFormador]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
-
     btnbuscarFormador.addEventListener("click",()=>{
-
         screenBuscarFormador.classList.remove("hiden")
         screenBuscarFormador.classList.add("show")
-        
-        screencadastrarFormador.classList.remove("show")
-        screencadastrarFormador.classList.add("hiden")
-
-        screendeletarFormador.classList.remove("show")
-        screendeletarFormador.classList.add("hiden")
-
-        screenlistarFormador.classList.remove("show")
-        screenlistarFormador.classList.add("hiden")
-
-        screenalterarFormador.classList.remove("show")
-        screenalterarFormador.classList.add("hiden")
-
+        titulopageFormadores.textContent = "Pesquisa de Formadores"
+        lista = [ screenlistarFormador,screencadastrarFormador, screendeletarFormador,screenalterarFormador]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
-
     btnlistarFormador.addEventListener("click",()=>{
-
         screenlistarFormador.classList.remove("hiden")
         screenlistarFormador.classList.add("show")
-        
-        screencadastrarFormador.classList.remove("show")
-        screencadastrarFormador.classList.add("hiden")
-
-        screendeletarFormador.classList.remove("show")
-        screendeletarFormador.classList.add("hiden")
-
-        screenBuscarFormador.classList.remove("show")
-        screenBuscarFormador.classList.add("hiden")
-
-        screenalterarFormador.classList.remove("show")
-        screenalterarFormador.classList.add("hiden")
-
+        titulopageFormadores.textContent = "Listagem de Formadores"
+        lista = [screenBuscarFormador,screencadastrarFormador, screendeletarFormador,screenalterarFormador]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
     btndeletarFormador.addEventListener("click",()=>{
-
         screendeletarFormador.classList.remove("hiden")
         screendeletarFormador.classList.add("show")
-        
-        screencadastrarFormador.classList.remove("show")
-        screencadastrarFormador.classList.add("hiden")
-
-        screenlistarFormador.classList.remove("show")
-        screenlistarFormador.classList.add("hiden")
-
-        screenBuscarFormador.classList.remove("show")
-        screenBuscarFormador.classList.add("hiden")
-
-        screenalterarFormador.classList.remove("show")
-        screenalterarFormador.classList.add("hiden")
-
+        titulopageFormadores.textContent = "Remoção de Formador"
+        lista = [screenBuscarFormador,screencadastrarFormador, screenlistarFormador,screenalterarFormador]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
     btnalterarFormador.addEventListener("click",()=>{
-
         screenalterarFormador.classList.remove("hiden")
         screenalterarFormador.classList.add("show")
-        
-        screencadastrarFormador.classList.remove("show")
-        screencadastrarFormador.classList.add("hiden")
-
-        screenlistarFormador.classList.remove("show")
-        screenlistarFormador.classList.add("hiden")
-
-        screenBuscarFormador.classList.remove("show")
-        screenBuscarFormador.classList.add("hiden")
-
-        screendeletarFormador.classList.remove("show")
-        screendeletarFormador.classList.add("hiden")
-
+        titulopageFormadores.textContent = "Actualização de Formador"
+        lista = [screenBuscarFormador,screencadastrarFormador, screenlistarFormador,screendeletarFormador]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
 
+    
+     //tela do gerenciamento dos alunos
+    const btncadastrarAluno = document.getElementById("cadastrarAluno")
+    const btnlistarAluno = document.getElementById("listarAluno")
+    const btnbuscarAluno = document.getElementById("buscarAluno")
+    const btndeletarAluno = document.getElementById("deletarAluno")
+    const btnalterarAluno = document.getElementById("alterarAluno")
+
+    const screencadastrarAluno = document.getElementsByClassName("cadastrarAluno")[0]
+    const screenlistarAluno = document.getElementsByClassName("listarAluno")[0]
+    const screenBuscarAluno = document.getElementsByClassName("BuscarAluno")[0]
+    const screendeletarAluno = document.getElementsByClassName("deletarAluno")[0]
+    const screenalterarAluno = document.getElementsByClassName("alterarAluno")[0]
+    const titulPageAluno = document.getElementById("titulPageAluno")
+
+    
+    btncadastrarAluno.addEventListener("click",()=>{
+        screencadastrarAluno.classList.remove("hiden")
+        screencadastrarAluno.classList.add("show")
+        titulPageAluno.textContent = "Cadastro de Aluno"
+        lista = [ screenlistarAluno,  screenBuscarAluno, screendeletarAluno,screenalterarAluno]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+    btnbuscarAluno.addEventListener("click",()=>{
+        screenBuscarAluno.classList.remove("hiden")
+        screenBuscarAluno.classList.add("show")
+        titulPageAluno.textContent = "Pesquisa de Aluno"
+        lista = [ screenlistarAluno,  screencadastrarAluno, screendeletarAluno,screenalterarAluno]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
+    btnlistarAluno.addEventListener("click",()=>{
+        screenlistarAluno.classList.remove("hiden")
+        screenlistarAluno.classList.add("show")
+        titulPageAluno.textContent = "Listagem de Aluno"
+        lista = [ screenBuscarAluno,  screencadastrarAluno, screendeletarAluno,screenalterarAluno]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
+    btndeletarAluno.addEventListener("click",()=>{
+        screendeletarAluno.classList.remove("hiden")
+        screendeletarAluno.classList.add("show")
+        titulPageAluno.textContent = "Remoção de Aluno"
+        lista = [ screenBuscarAluno,  screencadastrarAluno, screenlistarAluno,screenalterarAluno]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
+    btnalterarAluno.addEventListener("click",()=>{
+        screenalterarAluno.classList.remove("hiden")
+        screenalterarAluno.classList.add("show")
+        titulPageAluno.textContent = "Actualização de Aluno"
+        lista = [ screenBuscarAluno,  screencadastrarAluno, screenlistarAluno,screendeletarAluno]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
 
     //tela do pagamento
 
@@ -274,28 +380,38 @@ window.onload = function () {
     const salariobtn = document.getElementById("salario")
     const screenSalario = document.getElementById("salarial")
     const pagamentoTitle = document.getElementById("pagamentoTitle")
+
     paybtn.addEventListener("click",()=>{
-        screenverf.style.display = "none"
-        screenSalario.style.display = "none"
-        screenpay.style.display = "flex"
-        screenpay.style.flexDirection = "column"
+        screenpay.classList.remove("hiden")
+        screenpay.classList.add("show")
         pagamentoTitle.textContent = "Pagamento de Propina"
-    }) 
-    Pagametoverificar.addEventListener("click",()=>{
-            screenverf.style.display = "flex"
-            screenpay.style.display = "none"
-            screenSalario.style.display = "none"
-            screenverf.style.flexDirection ="column"
-            pagamentoTitle.textContent = "Verificação"
-        
+        lista = [ screenverf, screenSalario]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
+
+    Pagametoverificar.addEventListener("click",()=>{
+        screenverf.classList.remove("hiden")
+        screenverf.classList.add("show")
+        pagamentoTitle.textContent = "Verificação de Credenciais"
+        lista = [ screenpay, screenSalario]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
+    })
+
     salariobtn.addEventListener("click",()=>{
-            screenSalario.style.display = "flex"
-            screenverf.style.display = "none"
-            screenpay.style.display = "none"
-            screenverf.style.flexDirection ="column"
-            pagamentoTitle.textContent = "Salário do Formador"
-        
+        screenSalario.classList.remove("hiden")
+        screenSalario.classList.add("show")
+        pagamentoTitle.textContent = "Pagamento de Salário"
+        lista = [ screenverf, screenpay]
+        lista.forEach((iten)=>{
+            iten.classList.remove("show")
+            iten.classList.add("hiden")
+        })
     })
     
 }
